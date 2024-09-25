9656ea4caccdc271c7b6aad0e93a68f447b682b7
fazilcmohammed <140707546+fazilcmohammed@users.noreply.github.com>
2024-09-25 22:05:41 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'neutralDGrey': '#4D4D4D',
+        'brandPrimary': '#4CAF4F',
+        'neutralSilver': '#F5F7FA',
+        'neutralGrey': '#717171'
+      }
+    },
+  },
+  plugins: [],
+}
+
